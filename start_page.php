cf761deede85df62923a888ef671cf373ad316c6
bhargavbhegde7 <bhargavbhegde7@gmail.com>
2013-08-20 18:55:29 +0530

    repo created

---
+++ start_page.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>
+jsgame
+</title>
+<h1 align="center">Welcome to game</h1>
+</head>
+<body style="position:absolute; left:580px;">
+<p>please click below to play the game</p>
+<a href="pages/myjsgame1.html"> PLAY </a>
+</body>
+</html>
